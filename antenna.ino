332178c00a5f5d15d436c087c7488c46ae1d7277
Levi Lesches <levilesches@gmail.com>
2024-11-12 00:55:38 -0500

    Set up firmware template

---
+++ antenna.ino
@@ -0,0 +1,12 @@
+#include "src/utils/BURT_utils.h"
+
+// TODO: Add AntennaCommand, AntennaData, and Device.ANTENNA to burt_network
+// TODO: Add BurtSerial to communicate with the Dashboard
+
+void setup() {
+
+}
+
+void loop() {
+
+}
