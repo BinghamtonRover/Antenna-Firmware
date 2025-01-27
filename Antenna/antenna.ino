4589dcf75c4f98161cd480aa05052646b554b49e
ruari-r <ruarirandall@icloud.com>
2025-01-27 15:58:42 -0500

    Create and edit necessary files to control antenna

--- Antenna/antenna.ino
+++ Antenna/antenna.ino
@@ -1,12 +1,19 @@
 #include "src/utils/BURT_utils.h"
-#include "src/mars.pb.h"
+#include "src/tmc/BURT_TMC.h"
+#include "src/base_station.pb.h"
+#include "src/motor.pb.h"
+#include "src/utils/core.pb.h"
+#include "pinouts.h"
 
 // TODO: Add AntennaCommand, AntennaData, and Device.ANTENNA to burt_network
 // TODO: Add BurtSerial to communicate with the Dashboard
 
 #define DATA_SEND_INTERVAL 250 // ms
 
-BurtSerial serial(Device::Device_ANTENNA, handleCommand, AntennaData_fields, AntennaData_size);
+void handleCommand(const uint8_t* buffer, int length);
+void sendData();
+
+BurtSerial serial(Device::Device_ANTENNA, handleCommand, AntennaFirmwareData_fields, AntennaFirmwareData_size);
 BurtTimer dataTimer(DATA_SEND_INTERVAL, sendData);
 
 void setup() {
@@ -15,9 +22,10 @@ void setup() {
     Serial.println("Initializing software...");
     serial.setup();
     dataTimer.setup();
+    Serial.println("Done!");
 
     Serial.println("Initializing hardware...");
-    /*antenna.setup();*/
+    antenna.setup();
 
     Serial.println("MARS subsystem initialized");
 }
@@ -25,14 +33,36 @@ void setup() {
 void loop() {
     serial.update();
     dataTimer.update();
-    /*antenna.update();*/
+    antenna.update();
 }
 
-void handleCommand(const uint8_t* data, int length) {
-    auto command = BurtProto::decode<AntennaCommand>(data, length, AntennaCommand_fields);
-    /*antenna.handleCommand();*/
+MotorData getMotorData(StepperMotor& motor) {
+  return {
+    is_moving: motor.isMoving() ? BoolState::BoolState_YES : BoolState::BoolState_NO,
+    is_limit_switch_pressed: motor.limitSwitch.isPressed() ? BoolState::BoolState_YES : BoolState::BoolState_NO,
+    current_step: motor.currentSteps(),
+    target_step: motor.targetSteps(),
+    current_angle: (float) motor.currentPosition(),
+    target_angle: (float) motor.targetPosition()
+  };
 }
 
 void sendData() {
-    serial.send(/*&antenna.data*/)
+    MotorData data = MotorData_init_zero;
+    data = getMotorData(antenna);
+    serial.send(&data);
 }
+
+void handleCommand(const uint8_t* buffer, int length) {
+    auto command = BurtProto::decode<MotorCommand>(buffer, length, MotorCommand_fields);
+
+    // Debug: Move by Individual Steps
+    if (command.move_steps != 0) antenna.moveBySteps(command.move_steps);
+
+    // Precise Control: Move by # of Rad
+    if (command.move_radians != 0) antenna.moveBy(command.move_radians);
+
+    // IK Control: Move to Angle (Rad)
+    if (command.angle != 0) antenna.moveTo(command.angle);
+}
+
--- Antenna/pinouts.h
+++ Antenna/pinouts.h
@@ -1,4 +1,20 @@
 #ifndef PINOUTS_H
 #define PINOUTS_H
+#include "src/tmc/BURT_TMC.h"
+
+StepperMotorPins antennaPins = {
+	enable: 35,
+	chipSelect: 10,
+};
+
+StepperMotorConfig antennaConfig = {
+	name: "antenna",
+	current: 2000,
+	speed: 200'000,
+	acceleration: 200'000,
+	stepsPerUnit: microstepsPerRadian * 47,
+};
+
+StepperMotor antenna(antennaPins, antennaConfig);
 
 #endif
