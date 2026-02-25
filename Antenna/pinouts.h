0da6a37cf49d271aa09a391f4f4616bf87662b89
kenneth ou <kennethdacreator@gmail.com>
2026-02-25 11:51:14 -0500

    Refactored Stepper Sonstructors

--- Antenna/pinouts.h
+++ Antenna/pinouts.h
@@ -4,41 +4,34 @@
 #include "src/tmc/BURT_TMC.h"   // using internal ramp mode on step-dir-mode branch, not main branch
 
 
-StepperGeneralConfig swivelGeneralConfig = {
-  name: "swivel",
-  steps_per_unit: microsteps_per_rad * 47,
-};
+StepperMotor swivel(StepperGeneralConfig{
+  .name = "swivel",
+  .steps_per_unit = microsteps_per_rad * 47,
+},
 
-StepperMotorPins swivelMotorPins = {
-  chip_select: 10,   // for HREI RevA board- STEP_1
-};
+StepperMotorPins {
+  .chip_select = 10,   // for HREI RevA board- STEP_1
+},
 
-InternalRampConfig swivelInternalRampConfig = {
-    current: 2000,
-    speed: 200'000,
-    acceleration: 200'000,
-};
+InternalRampConfig {
+    .current = 2000,
+    .speed = 200'000,
+    .acceleration = 200'000,
+});
 
+StepperMotor pitch(StepperGeneralConfig {
+  .name = "pitch",
+  .steps_per_unit = microsteps_per_rad * 47,
+},
 
+StepperMotorPins{
+    .chip_select = 37,   // for HREI RevA board- STEP_2
+},
 
-StepperGeneralConfig pitchGeneralConfig = {
-  name: "pitch",
-  steps_per_unit: microsteps_per_rad * 47,
-};
-
-StepperMotorPins pitchMotorPins = {
-    chip_select: 37,   // for HREI RevA board- STEP_2
-};
-
-InternalRampConfig pitchInternalRampConfig = {
-    current: 2000,
-    speed: 200'000,
-    acceleration: 200'000,
-};
-
-
-
-StepperMotor swivel(swivelGeneralConfig, swivelMotorPins, swivelInternalRampConfig);
-StepperMotor pitch(pitchGeneralConfig, pitchMotorPins, pitchInternalRampConfig);
+InternalRampConfig{
+    .current = 2000,
+    .speed = 200'000,
+    .acceleration = 200'000,
+});
 
 #endif
